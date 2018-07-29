ac71ab6ab082e9044506046a0678452af10d47ad
w4tsw4ts <billy.montgomery@ge.com>
2018-07-29 15:05:56 -0400

    Update v1

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # unit-4-game
+
+This is a simple game that gives you a random number at the beginnig of the game.  We will call this number your goal number.  
+
+You will then have 4 crystals to pick from.  Each crystal has a random number assigned to it that you cannot see.  
+
+Your mission is to click on the crystals to get their random numbers to add up to the same number as the goal number given to you at the start of the game.  If you your total crystal numbers goes over the goal number you lose. 
+
+
+
+Why the project is useful
+How users can get started with the project
+Where users can get help with your project
+Who maintains and contributes to the project
---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+//Define variables
+var targetNumber = 0;
+var crystals = [];
+var odd = 0;
+var even = 0;
+var Wins = 0;
+var Loss = 0;
+var totalScore = 0;
+
+function resetGame() {
+    //Assign Random numbers
+    targetNumber = Math.floor((Math.random() * 120 - 19) + 19);
+    for (x = 0; x !== 4; ++x ) {
+        crystals[x] = Math.floor((Math.random() * 12) + 1);
+    }
+    
+    // Test to see if crystal has at least one odd or even number.
+    even = 0;
+    odd = 0;
+    for (x = 0; x !== 4; x++) {
+        if (testEven(crystals[x]) == true) {
+            even++
+            console.log("crystal" + x + " and has a value of " + crystals[x] + " is and even number.");
+        } else {
+            odd++
+            console.log("crystal" + x + " and has a value of " + crystals[x] + " is and odd number.");
+        }
+    }
+    
+    totalScore =0;
+}
+
+function testEven(num) {
+    if ( num % 2 == 0) {
+        return true;
+    } 
+}
+
+function stats() {
+
+}
+
+// Start Game
+resetGame();
+// Validate we have at least one even and one odd value crystal
+while (odd === 0 || even === 0) {
+    console.log("we do NOT have at least one even and one odd value crystal");
+    resetGame();
+}
+// Is at least 1 of the crystals odd or one even.
+
+console.log("Target Number = " + targetNumber);
+console.log("Crystal 1 Value = " + crystals[0]);
+console.log("Crystal 2 Value = " + crystals[1]);
+console.log("Crystal 3 Value = " + crystals[2]);
+console.log("Crystal 4 Value = " + crystals[3]);
+console.log("Your total score = " + totalScore);
