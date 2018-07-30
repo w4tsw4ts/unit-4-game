0fc640f3114315eb6dd28977f07777c2fc0289d6
w4tsw4ts <billy.montgomery@ge.com>
2018-07-29 20:42:28 -0400

    Update v2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,8 @@ var totalScore = 0;
 
 function resetGame() {
     //Assign Random numbers
+    targetNumber = 0;
+    crystals = [];
     targetNumber = Math.floor((Math.random() * 120 - 19) + 19);
     for (x = 0; x !== 4; ++x ) {
         crystals[x] = Math.floor((Math.random() * 12) + 1);
@@ -40,14 +42,53 @@ function stats() {
 
 }
 
+function checkGame() {
+    if (totalScore === targetNumber) {
+        Wins++;
+        console.log("You WIN!!");
+        console.log("Wins: " + Wins);
+        console.log("Losses: " + Loss);
+        resetGame();
+    } else if (totalScore > targetNumber) {
+        Loss++;
+        console.log("Sorry you lost.");
+        console.log("Wins: " + Wins);
+        console.log("Losses: " + Loss);
+        resetGame();
+    } else {
+        console.log("New totalScore: " + totalScore + " Target #: " + targetNumber);
+    }
+}
+
 // Start Game
 resetGame();
 // Validate we have at least one even and one odd value crystal
 while (odd === 0 || even === 0) {
-    console.log("we do NOT have at least one even and one odd value crystal");
+    console.log("We do NOT have at least one even and one odd value crystal");
     resetGame();
 }
-// Is at least 1 of the crystals odd or one even.
+// Validate we have unique numbers
+
+$(document).ready(function() {
+  $("#crystal0").on("click", function() {
+      totalScore = totalScore + crystals[0];
+      checkGame();
+  });
+
+  $("#crystal1").on("click", function() {
+      totalScore = totalScore + crystals[1];
+      checkGame();
+  });
+
+  $("#crystal2").on("click", function() {
+      totalScore = totalScore + crystals[2];
+      checkGame();
+  });
+
+  $("#crystal3").on("click", function() {
+      totalScore = totalScore + crystals[3];
+      checkGame();
+  });
 
 console.log("Target Number = " + targetNumber);
 console.log("Crystal 1 Value = " + crystals[0]);
@@ -55,3 +96,6 @@ console.log("Crystal 2 Value = " + crystals[1]);
 console.log("Crystal 3 Value = " + crystals[2]);
 console.log("Crystal 4 Value = " + crystals[3]);
 console.log("Your total score = " + totalScore);
+console.log("Wins: " + Wins);
+console.log("Losses: " + Loss);
+});
